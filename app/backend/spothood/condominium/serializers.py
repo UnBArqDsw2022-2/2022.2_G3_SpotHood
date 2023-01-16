bf7e82dfdd86034baf7920b3a2da5471649f6e2a
Joao Henrique <joao.henrique1299@hotmail.com>
2023-01-16 01:29:46 -0300

    [feat](#74): Adicionando validacoes aos campos da classe PessoaSerializer
    Co-authored-by: Rafael Berto <rafaelbertopereira@gmail.com>

--- app/backend/spothood/condominium/serializers.py
+++ app/backend/spothood/condominium/serializers.py
@@ -1,7 +1,46 @@
 from rest_framework import serializers
 from .models import Pessoa
 
+import re
+from datetime import datetime
+
 class PessoaSerializer(serializers.HyperlinkedModelSerializer):
+    def validate_nome(self,value):
+        value = value.strip()
+        quantia_digitos = len(re.findall(r'\d+',value))
+
+        if  quantia_digitos > 0:
+            raise serializers.ValidationError("Nome inválido, não devem existir números em nomes.")
+
+    def validate_cpf(self,value):
+        value = value.strip()
+        cpf_valido = re.findall(r'^\d{3}.d{3}.d{3}-d{2}$',value)
+
+        if not cpf_valido:
+            raise serializers.ValidationError("Cpf inválido, formato não aceito.")
+
+    def validate_email(self,value):
+        value = value.strip()
+        email_valido = re.findall(r'^.+@[a-zA-Z]+\.[a-zA-Z]+$',value)
+
+        if not email_valido:
+            raise serializers.ValidationError("E-mail inválido, formato não aceito.")
+
+    def validate_telefone(self,value):
+        value = value.strip()
+        telefone_valido = re.findall(r'^\(\d{2}\)\ \d{5}-\d{4}$',value)
+
+        if not telefone_valido:
+            raise serializers.ValidationError("Telefone inválido, formato não aceito.")
+
+    def validate_data_nascimento(self,value):
+        hoje = datetime.now().strftime("%Y-%m-%d")
+        value = value.strftime("%Y-%m-%d")
+
+        if hoje < value:
+            raise serializers.ValidationError("Data inválida, impossível criar usuário que não nasceu ainda.")
+
+
     class Meta:
         model = Pessoa
         fields = ('cpf',
