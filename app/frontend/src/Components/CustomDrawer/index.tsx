b6300f92f61ef5d0de1e5d24546f3575eeea798a
Victor Yukio <yukio-mac@MacBook-Air-de-Victor.local>
2023-01-29 19:53:10 -0300

    [frontend] - finalizador a navbar
    Signed-off-by: Victor Yukio <yukio-mac@MacBook-Air-de-Victor.local>
    Co-authored-by: Joao Henrique <joao.henrique1299@hotmail.com>
    Co-authored-by: Rocsantos <ro12062000@gmail.com>

--- app/frontend/src/Components/CustomDrawer/index.tsx
+++ app/frontend/src/Components/CustomDrawer/index.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { Box, Drawer, IconButton, makeStyles, Typography } from "@mui/material";
+import { Box, Drawer, IconButton, Link, makeStyles } from "@mui/material";
 import Icon from "../Icons";
 import { OptionContainer, PhotoContainer } from "./style";
+import { useNavigate } from "react-router-dom";
 
 interface Props {}
 
 const CustomDrawer: React.FC<Props> = ({}) => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -21,37 +24,69 @@ const CustomDrawer: React.FC<Props> = ({}) => {
         <Icon
           name="menu"
           action={() => null}
+          style={{
+            fontSize: "25px",
+          }}
         />
       </IconButton>
       <Drawer
         anchor="left"
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
-        classes={{  }}
+        style={{
+          backgroundColor: "8B8B8B"
+        }}
       >
-        <Box p={2} width="250px" textAlign="center" role="presentation">
+        <Box width="250px" textAlign="center" role="presentation">
           <PhotoContainer>
-
+            <Icon
+              name="account-circle"
+              action={() => null}
+              style={{ fontSize: "15rem" }}
+            />
+            <h2>Nome da pessoa</h2>
+            <Link
+              onClick={() => {navigate("/")}}
+            >
+              Acessar perfil
+            </Link>
           </PhotoContainer>
-          <OptionContainer>
-            teste
+          <OptionContainer
+            style={{
+              fontWeight: "bold"
+            }}
+            onClick={() => {navigate("/")}}
+          >
+            Aviso
+          </OptionContainer>
+          <OptionContainer
+            style={{
+              fontWeight: "bold"
+            }}
+            onClick={() => {navigate("/")}}
+          >
+            Reserva
+          </OptionContainer>
+          <OptionContainer
+            style={{
+              fontWeight: "bold"
+            }}
+            onClick={() => {navigate("/")}}
+          >
+            Meu Imóvel
+          </OptionContainer>
+          <OptionContainer
+            style={{
+              fontWeight: "bold"
+            }}
+            onClick={() => {navigate("/")}}
+          >
+            Sair
           </OptionContainer>
         </Box>
       </Drawer>
     </>
   );
 };
-
-const useStyles = makeStyles({
-  list: {
-    width: 250
-  },
-  fullList: {
-    width: "auto"
-  },
-  paper: {
-    background: "blue"
-  }
-});
 
 export { CustomDrawer };
--- app/frontend/src/Components/Icons/index.tsx
+++ app/frontend/src/Components/Icons/index.tsx
@@ -27,72 +27,73 @@ import React from "react";
 
 interface Props {
   name: string;
+  style: React.CSSProperties;
 
   action: Dispatch<SetStateAction<any>>;
 }
 
-const Icon: React.FC<Props> = ({ name, action }) => {
+const Icon: React.FC<Props> = ({ name, action, style }) => {
   switch (name) {
-    case 'dns': return <Dns onClick={action} />
-    case 'storefront': return <Storefront onClick={action} />
-    case 'account-tree': return <AccountTree onClick={action} />
-    case 'home-work': return <HomeWork onClick={action} />
-    case 'blur-on': return <BlurOn onClick={action} />
-    case 'expand_more': return <ExpandMore onClick={action} />
-    case 'expand_less': return <ExpandLess onClick={action} />
-    case 'menu': return <Menu onClick={action} />
-    case 'menu-open': return <MenuOpen onClick={action} />
-    case 'account-circle': return <AccountCircle onClick={action} />
-    case 'apps': return <Apps onClick={action} />
-    case 'arrow-back': return <ArrowBack onClick={action} />
-    case 'arrow-forward': return <ArrowForward onClick={action} />
-    case 'close': return <Close onClick={action} />
-    case 'school': return <School onClick={action} />
-    case 'logout': return <Logout onClick={action} />
-    case 'notifications': return <Notifications onClick={action} />
-    case 'event-busy': return <EventBusy onClick={action} />
-    case 'chevron-left': return <ChevronLeft onClick={action} />
-    case 'chevron-right': return <ChevronRight onClick={action} />
-    case 'check-box-blank': return <CheckBoxOutlineBlank onClick={action} />
-    case 'check-box': return <CheckBox onClick={action} />
-    case 'double-arrow-right': return <DoubleArrow onClick={action} />
-    case 'menu-book': return <MenuBook onClick={action} />
-    case 'warning': return <Warning onClick={action} />
-    case 'info-icon': return <Info onClick={action} />
-    case 'dangerous-icon': return <Dangerous onClick={action} />
-    case 'check-circle-icon': return <CheckCircle onClick={action} />
-    case 'security': return <Security onClick={action} />
-    case 'route': return <Route onClick={action} />
-    case 'calendar': return <CalendarMonth onClick={action} />
-    case 'done': return <Done onClick={action} />
-    case 'edit': return <Edit onClick={action} />
-    case 'folder': return <Folder onClick={action} />
-    case 'folder-create': return <CreateNewFolder onClick={action} />
-    case 'folder-special': return <FolderSpecial onClick={action} />
-    case 'file': return <Description onClick={action} />
-    case 'file-download': return <FileDownload onClick={action} />
-    case 'file-upload': return <FileUpload onClick={action} />
-    case 'recording': return <EmergencyRecording onClick={action} />
-    case 'timeline': return <Timeline onClick={action} />
-    case 'video': return <VideoLibrary onClick={action} />
-    case 'book': return <Book onClick={action} />
-    case 'pending-actions': return <PendingActions onClick={action} />
-    case 'star': return <Star onClick={action} />
-    case 'settings': return <Settings onClick={action} />
-    case 'engineering': return <Engineering onClick={action} />
-    case 'schedule': return <Schedule onClick={action} />
-    case 'link': return <Link onClick={action} />
-    case 'key': return <Key onClick={action} />
-    case 'add-circle': return <AddCircle onClick={action} />
-    case 'add': return <Add onClick={action} />
-    case 'delete': return <Delete onClick={action} />
-    case 'remove': return <Remove onClick={action} />
-    case 'cancel': return <Cancel onClick={action} />
-    case 'reader': return <ChromeReaderMode onClick={action} />
-    case 'assigment': return <Assignment onClick={action} />
-    case 'toggle-on': return <ToggleOn onClick={action} />
-    case 'toggle-off': return <ToggleOff onClick={action} />
-    default: return <HelpOutline onClick={action} />
+    case 'dns': return <Dns onClick={action} style={style} />
+    case 'storefront': return <Storefront onClick={action} style={style} />
+    case 'account-tree': return <AccountTree onClick={action} style={style} />
+    case 'home-work': return <HomeWork onClick={action} style={style} />
+    case 'blur-on': return <BlurOn onClick={action} style={style} />
+    case 'expand_more': return <ExpandMore onClick={action} style={style} />
+    case 'expand_less': return <ExpandLess onClick={action} style={style} />
+    case 'menu': return <Menu onClick={action} style={style} />
+    case 'menu-open': return <MenuOpen onClick={action} style={style} />
+    case 'account-circle': return <AccountCircle onClick={action} style={style} />
+    case 'apps': return <Apps onClick={action} style={style} />
+    case 'arrow-back': return <ArrowBack onClick={action} style={style} />
+    case 'arrow-forward': return <ArrowForward onClick={action} style={style} />
+    case 'close': return <Close onClick={action} style={style} />
+    case 'school': return <School onClick={action} style={style} />
+    case 'logout': return <Logout onClick={action} style={style} />
+    case 'notifications': return <Notifications onClick={action} style={style} />
+    case 'event-busy': return <EventBusy onClick={action} style={style} />
+    case 'chevron-left': return <ChevronLeft onClick={action} style={style} />
+    case 'chevron-right': return <ChevronRight onClick={action} style={style} />
+    case 'check-box-blank': return <CheckBoxOutlineBlank onClick={action} style={style} />
+    case 'check-box': return <CheckBox onClick={action} style={style} />
+    case 'double-arrow-right': return <DoubleArrow onClick={action} style={style} />
+    case 'menu-book': return <MenuBook onClick={action} style={style} />
+    case 'warning': return <Warning onClick={action} style={style} />
+    case 'info-icon': return <Info onClick={action} style={style} />
+    case 'dangerous-icon': return <Dangerous onClick={action} style={style} />
+    case 'check-circle-icon': return <CheckCircle onClick={action} style={style} />
+    case 'security': return <Security onClick={action} style={style} />
+    case 'route': return <Route onClick={action} style={style} />
+    case 'calendar': return <CalendarMonth onClick={action} style={style} />
+    case 'done': return <Done onClick={action} style={style} />
+    case 'edit': return <Edit onClick={action} style={style} />
+    case 'folder': return <Folder onClick={action} style={style} />
+    case 'folder-create': return <CreateNewFolder onClick={action} style={style} />
+    case 'folder-special': return <FolderSpecial onClick={action} style={style} />
+    case 'file': return <Description onClick={action} style={style} />
+    case 'file-download': return <FileDownload onClick={action} style={style} />
+    case 'file-upload': return <FileUpload onClick={action} style={style} />
+    case 'recording': return <EmergencyRecording onClick={action} style={style} />
+    case 'timeline': return <Timeline onClick={action} style={style} />
+    case 'video': return <VideoLibrary onClick={action} style={style} />
+    case 'book': return <Book onClick={action} style={style} />
+    case 'pending-actions': return <PendingActions onClick={action} style={style} />
+    case 'star': return <Star onClick={action} style={style} />
+    case 'settings': return <Settings onClick={action} style={style} />
+    case 'engineering': return <Engineering onClick={action} style={style} />
+    case 'schedule': return <Schedule onClick={action} style={style} />
+    case 'link': return <Link onClick={action} style={style} />
+    case 'key': return <Key onClick={action} style={style} />
+    case 'add-circle': return <AddCircle onClick={action} style={style} />
+    case 'add': return <Add onClick={action} style={style} />
+    case 'delete': return <Delete onClick={action} style={style} />
+    case 'remove': return <Remove onClick={action} style={style} />
+    case 'cancel': return <Cancel onClick={action} style={style} />
+    case 'reader': return <ChromeReaderMode onClick={action} style={style} />
+    case 'assigment': return <Assignment onClick={action} style={style} />
+    case 'toggle-on': return <ToggleOn onClick={action} style={style} />
+    case 'toggle-off': return <ToggleOff onClick={action} style={style} />
+    default: return <HelpOutline onClick={action} style={style} />
   }
 }
 
