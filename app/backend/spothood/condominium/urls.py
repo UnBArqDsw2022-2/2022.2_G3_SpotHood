bd8699a49181296f7bc9e27538807096205a516b
Fernando Vargas <nandovargas7@gmail.com>
2023-01-29 02:35:56 -0300

    [feat](#71) Criacao do endpoint de login

--- app/backend/spothood/condominium/urls.py
+++ app/backend/spothood/condominium/urls.py
@@ -1,13 +1,13 @@
 from rest_framework import routers
-
-from .views import PessoaViewSet, CondominioViewSet, AvisoViewSet,\
+from django.urls import path
+from .views import PessoaViewSet, CondominioViewSet, AvisoViewSet, LoginViewSet,\
                    ImovelViewSet, EspacoHabitacionalViewSet, ReservaEspacoHabitacionalViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pessoa', PessoaViewSet)
+router.register(r'login', LoginViewSet, basename='login')
 router.register(r'condominio', CondominioViewSet)
 router.register(r'aviso', AvisoViewSet)
 router.register(r'imovel', ImovelViewSet)
 router.register(r'espaco-habitacional', EspacoHabitacionalViewSet)
 router.register(r'reservaespacos', ReservaEspacoHabitacionalViewSet)
-
