f23f0eb93a485bf71a3cc20adb07c3bcce5e4dc8
MarianaPRio <marianapires00@gmail.com>
2023-01-30 20:41:23 -0300

    Adicionando rotas e alteracoes no cadastro de condominio
    Signed-off-by: MarianaPRio <marianapires00@gmail.com>

--- app/frontend/src/Pages/Building/index.tsx
+++ app/frontend/src/Pages/Building/index.tsx
@@ -3,11 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { Page, Container, Title, Form, ButtonDiv, SmallInput } from './style';
 import { BigIconButton } from "../../Components/BigIconButton";
 import addIcon from "../../assets/addCond.png";
+import closeIcon from "../../assets/closeIcon.png";
 import logo from "../../assets/SpotHood-logo.png";
 
 const Building = () => {
   const navigate = useNavigate();
   const addBtnHoverColor = [100, 196, 111];
+  const closeBtnHoverColor = [134, 139, 247];
 
   const cadastroIn = () => {
     navigate("/cadastrar-imovel");
@@ -16,20 +18,32 @@ const Building = () => {
   return (
     <Page>
       <Container>
-      <Title>Imóveis do Condomínio</Title>
-      <BigIconButton
-            src={addIcon}
-            alt="primary"
-            label={["Adicionar","imovel"]}
-            color="#41AD4C"
-            hoverColor={addBtnHoverColor}
-            onClick={() => cadastroIn()}
-            style={{
-                width:"20%"
-              }}
-        />        
-        <div><img src={logo} alt="spothood-logo" width="70%" />
-        </div>
+        <Title>Imóveis do Condomínio</Title>
+          <div>
+          <BigIconButton
+                src={addIcon}
+                alt="primary"
+                label={["Adicionar","imovel"]}
+                color="#41AD4C"
+                hoverColor={addBtnHoverColor}
+                onClick={() => cadastroIn()}
+                style={{
+                    width:"30%"
+                  }}
+          />
+          <BigIconButton
+                src={closeIcon}
+                alt="primary"
+                label={["Adicionar","imovel"]}
+                color="#a0a0a0"
+                hoverColor={closeBtnHoverColor}
+                onClick={() => cadastroIn()}
+                style={{
+                    width:"30%"
+                }}
+          />
+          </div>        
+        <div><img src={logo} alt="spothood-logo" width="70%" /></div>
       </Container>
     </Page>
   );
