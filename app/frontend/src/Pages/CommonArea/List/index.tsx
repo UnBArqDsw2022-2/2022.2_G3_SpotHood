54da2eaf128aeb76bd25acd68e5802d36f002a1e
Rodrigo Santos <ro12062000@gmail.com>
2023-01-30 23:35:28 -0300

    [fe02](#95) Add Button HousingAreas

--- app/frontend/src/Pages/CommonArea/List/index.tsx
+++ app/frontend/src/Pages/CommonArea/List/index.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { Page, Container, Title, ButtonDiv } from './style';
 import logo from "../../../assets/SpotHood-logo.png";
+import joinCond from "../../../assets/joinCond.png";
 import { spotHoodService } from "../../../Services/spotHoodServices";
 import { useState, useEffect } from 'react';
 import { BigIconButton } from "../../../Components/BigIconButton";
@@ -29,16 +30,26 @@ const HousingArea = () => {
 		}
 		getCondInfo();
 	},[])
-	housingAreas.map(area=>{
-		console.log(area)
-	})
   return (
     <Page>
       <Container>
         <Title>Espaços Habitacionais do Condomínio</Title>
         <ButtonDiv>
 					{
-						housingAreas.map(area=> <div>{area.nome}</div>)
+						housingAreas.map(area=> {
+						return (
+							<BigIconButton
+								src={joinCond}
+								alt="primary"
+								label={area.nome}
+								color="lightgreen"
+								hoverColor={[100, 196, 111]}
+								onClick={ ()=>alert("Cadastrou") }
+								style={{
+									width:"100%"
+								}}
+							/>
+						)})
 					}
 				</ButtonDiv>
         <div><img src={logo} alt="spothood-logo" width="60%" /></div>
